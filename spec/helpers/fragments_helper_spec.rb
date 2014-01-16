48a63684e7f93540cf8c8cc4125db8bf335d91da
José Bonnet <jose.bonnet@icloud.com>
2014-01-16 14:35:56 +0000

    Put everything in synch

--- spec/helpers/fragments_helper_spec.rb
+++ spec/helpers/fragments_helper_spec.rb
@@ -1,15 +1,38 @@
 require 'spec_helper'
+require 'wiki_words'
 
-# Specs in this file have access to a helper object that includes
-# the FragmentsHelper. For example:
-#
-# describe FragmentsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe FragmentsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validates a WikiWord" do
+    it "if it starts w/ a capital letter, followed by at least one lower case letter, followed by any number of blocks like the first one" do
+      str = "ThisIsWikiWord"
+      expect(str).to match(/#{WikiWords::WIKI_WORD_PATTERN}/)
+    end
+    it "if it starts with '[[', followed by any number of characters and ending w/ ']]" do
+      str = "[[This, is thré page name]]"
+      expect(str).to match(/\[\[(.*)\]\]/)
+      puts "Matched: #{Regexp.last_match(1)}" 
+    end
+    it "if it starts with '[[', followed by any number of characters, a '|', a text o be shown and ending w/ ']]'" do
+      str = "[[This, is thé page name|This is the text]]"
+      link, text = /\[\[(?<link>.*)\]\]/.match(str)[:link].split('|')[1..2]
+      puts "link: #{link}, text: #{text}"
+
+    end
+  end
+
+  context "Does not validate a WikiWord" do
+    it "if it starts w/ a back-slash, followed by a capital letter, followed by at least one lower case letter, followed by any number of blocks like the previous one" do
+      str = "\\ThisShouldNotBeWikiWord"
+      #expect(str).not_to match(/#{WikiWords::WIKI_WORD_PATTERN}/)
+      pending "To do!"
+    end
+    it "if it starts w/ two capital letters, followed by any combination of characters" do
+      str = "TWoConsecutiveCapitalLettersDontMakeaWikiWord"
+      pending "To do!"
+    end
+    it "if it has a non alpha-numeric character" do
+      str = "ThisIsn'tWikiWord"
+      pending "To do!"
+    end
+  end
 end
